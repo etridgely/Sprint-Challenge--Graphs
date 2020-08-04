9afc043bc0e22be201c2706c93387e6a25f36a4b
etridgely <5757423+etridgely@users.noreply.github.com>
2020-08-03 19:51:50 -0700

    finished

--- adv.py
+++ adv.py
@@ -27,9 +27,96 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+# Pull the Stack class
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
 traversal_path = []
+# Create a dictionary for this adventure, it'll help
+graph = {}
+# Create a Stack from the class above
+stack = Stack()
+# Create a set to record where our player will have gone
+visited_rooms = set()
 
+# Transform the graph into a dictionary
+for i in range(len(room_graph)):
+    graph[i] = {}
 
+# The graph has no more than 500 rooms. Therefore, we can set this as a limit while traversing.
+while len(visited_rooms) < 500:
+    # Create a variable for where our player is
+    current_room = player.current_room
+    # Map out the location to where that room is in our dictionary
+    current_vertex = graph[current_room.id]
+    # For any room we do not see, add them to our set
+    if current_room.id not in visited_rooms:
+        visited_rooms.add(current_room.id)
+
+    # Identify all the exits in our current room using the get_exits method established in room.py
+    exits = [direction for direction in current_room.get_exits()]
+
+    for direction in exits:
+        if direction not in current_vertex:
+            # If our desired direction is not in the location, put a placeholder there.
+            current_vertex[direction] = '?'
+
+    # If the placeholder cannot be found, then let's go ahead and remove that bad boy from our Stack.
+    # Then we'll add the direction to our path.
+    # Finally, update the player's direction as they move along
+    if '?' not in current_vertex.values():
+        direction = stack.pop()
+        traversal_path.append(direction)
+        player.current_room = current_room.get_room_in_direction(direction)
+
+    else:
+        for direction in exits:
+            if current_vertex[direction] == '?':
+                # Using the next room in the place, let's update using our placeholder
+                next_room = current_room.get_room_in_direction(direction)
+                # Create an opposite direction
+                opposite_direction = ''
+                # Add the next vertex to our dictionary
+                next_vertex = graph[next_room.id]
+                #map out the directions so we know what's possible
+                if direction == 'n':
+                    opposite_direction = 's'
+                
+                elif direction == 's':
+                    opposite_direction = 'n'
+                
+                elif direction == 'e':
+                    opposite_direction = 'w'
+                
+                elif direction == 'w':
+                    opposite_direction = 'e'
+                
+                # Move onto the next one
+                current_vertex[direction] = next_room.id
+                next_vertex[opposite_direction] = current_room.id
+                player.current_room = next_room
+
+                # Add the direction to our path
+                traversal_path.append(direction)
+                # Add the opposite direction to our Stack
+                stack.push(opposite_direction)
+                
+                break
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
